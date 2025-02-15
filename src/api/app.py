28134140a75cd408d82ce55b1d0cebed1c4ec57d
Adrian Joost <adi.joost@gmail.com>
2025-02-16 00:28:43 +0100

    added swagger

--- src/api/app.py
+++ src/api/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_restful import Api
-
+from flasgger import Swagger
+from config.rootPath import getRootPath
 from src.resources.pumpResource import PumpResource
 
 app = Flask(__name__)
 api = Api(app)
+
+swaggerPath = str(getRootPath().joinpath("src/api/swagger.yaml"))
+swagger = Swagger(app, template_file=swaggerPath)
+
 
 @app.route("/")
 def hello():
